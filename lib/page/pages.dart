d5a757a114078be24d79e9e96a07e1a4df3e5b88
castariva18 <tengku13elmiro@gmail.com>
2021-10-02 16:24:25 +0700

    Initial Commit

---
+++ lib/page/pages.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+part 'home_page.dart';
---
+++ lib/shared/theme.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+//Color
+Color whiteColor = "FFFFFF".toColor();
+
+//Font
+TextStyle whiteHeaderFontStyle = GoogleFonts.poppins().copyWith(
+    color: "FFFFFF".toColor(), fontWeight: FontWeight.w400, fontSize: 20);
